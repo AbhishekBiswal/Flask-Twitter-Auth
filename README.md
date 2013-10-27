0f859d201188a7c5db2ee1c7f7e9a369e5288d65
Abhishek Biswal <ab@ab.(none)>
2013-10-27 20:11:14 +0530

    initial push

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# flask-twitter
+# Flask Example App using Twitter Authentication to Login Users.
+## By (Abhishek Biswal)[http://abhishekbiswal.com]
+
+### Required Packages
+* Flask ( ofcourse )
+* flask-oauth
+
+Install Flask-OAuth using this command:
+
+'''pip install flask-oauth'''
--- app.py
+++ app.py
@@ -1,35 +1,35 @@
-import os
-import json
-from flask import Flask
-from flask import render_template, send_from_directory, Response, url_for, request
+# Abhishek Biswal
+# http://abhishekbiswal.com
 
-# app initialization
-app = Flask(__name__)
-app.config.update(
-	DEBUG = True,
-	SECRET_KEY = 'e6373add09271e1087155c903f0a593012399435136372cc'
+from flask import Flask, session, url_for, render_template, request, redirect
+from flask_oauth import OAuth
+
+oauth = OAuth()
+
+# Set up twitter OAuth client
+twitter = oauth.remote_app('twitter',
+    base_url          = 'https://api.twitter.com/1/',
+    request_token_url = 'https://api.twitter.com/oauth/request_token',
+    access_token_url  = 'https://api.twitter.com/oauth/access_token',
+    authorize_url     = 'https://api.twitter.com/oauth/authenticate',
+    consumer_key      = 'YOUR_CONSUMER_KEY',
+    consumer_secret   = 'YOUR_CONSUMER_SECRET'
 )
 
-# controllers
-@app.route('/')
-def index():
-	return render_template('index.html')
+# returns a tuple of twitter tokens, if they exist
+@twitter.tokengetter
+def get_twitter_token(token=None):
+	return session.get('twitter_token')
 
-@app.route('/about')
-def about():
-	return render_template('about.html')
+@app.route('/login')
+def login():
+	return twitter.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next'), _external=True))
 
-# special file handlers
-@app.route('/favicon.ico')
-def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static'), 'img/favicon.ico')
-
-# error handlers
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'), 404
-
-# server launchpad
-if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+@app.route('/oauth_authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+	next_url = request.args.get('next') or url_for('index')
+	if resp is None:
+		return "Error Occured."
+	return str(resp) # Prints the output. From here you can either insert the info into the database or directly create a session using the info.
